5cdaef382b35c8450ba6c5f28c728357bfcd6427
Mohsin Rahman <mohsinrahmang@gmail.com>
2019-11-07 13:11:03 +0100

    Update README.md
    Report part

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# TechStore-N
+# TechStore
 Project Grupp 6
+# Shopping cart using JS
+
+## Introduction:
+	This project is created using Html5, Css, ES6 (Javascript) and Local storage.
+We have stored our initial data in json file. Fetch API has been used to retrieve data from json file.
+We have used OO concepts to develop this cart system. Project is divided into three classes named Phone, Display and Storage.
+  
+### Phone:
+In Phone Class,  we have used fetch API to retrieve data from json file.
+### Display:
+Display class is mainly focusing on display the phones(image, title. price) on interface. displayPhones, addToCart and setCartValues are the main functions that we have created to perform certain operations.
+### Storage:
+Storage Class is used to store, receive and update  data in local database. To perform these operations we have created 6 different functions. Name of these functions are db, getUserItem, saveCart, getSaveCart, saveTotalAmount, getTotalAmount.
+
+## Fetch API: 
+Fetch Api is a one of the method to fetch resources from DB.There were other possible ways e.g JQuery and Axios to perform fetching task.
+
+## Difficulties to develop the Project: 
+There were a couple of difficulties during the development process.
+ 1: It was difficult to decide the architecture of the whole system e.g how many classes should we have? What will be the methods or functions should we have?
+2: Data flow was another thing that took time to understand?
+
+## Tools & technologies:
+
+Below is a list of tools and technologies that we have used for development:
+Html/CSS
+Flex/Bootstrap
+Javascript
+Git/Github
+
+
+
--- main.js
+++ main.js
@@ -1,31 +1,134 @@
-var listOfProducts;
+const phoneDOM = document.querySelector(".mobiles");
+const cartDOM = document.querySelector(".cart img");
+const counter = document.querySelector(".counter");
+let cart = [];
+let DOMbutton = [];
 
-/** Get products from the json file and store it in a gobal variable */
-function loadProducts() {
-    fetch("./products.json")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(products) {
-        listOfProducts = products;
-        addProductsToWebpage();
-    });
+class Phone{
+   async getPhones(){
+       try{
+           let result = await fetch("products.json");
+           let data = await result.json();           
+           return data;
+       }
+       catch(error){
+           console.log(error);
+       }
+    }
 }
 
+class Display{
+   
+    displayPhones(phones){
+        let result =" ";
+        let style = {
+            border: "none",
+            borderBottom: "1px solid #F5F5F5",
+            textAlign: "center",
+            background: "ffffff",
+            fontSize:"bold",
+            bgcolor:"#F5F5F5"
+        };
+        
+        
+        cartDOM.addEventListener('click', () =>{
+            cartDisplay();
+        });
 
-function initSite() {
-    loadProducts();
-    // This would also be a good place to initialize other parts of the UI
-}
+        function cartDisplay(){
+            
+           let finalCart =  Storage.getSaveCart();
+            
+           result =' ';
+           console.log(finalCart);
+            
 
-/** Uses the loaded products data to create a visible product list on the website */
-function addProductsToWebpage() {
-    // Check your console to see that the products are stored in the listOfProducts varible.
-    console.log(listOfProducts);
 
-    // Add your code here, remember to brake your code in to smaller function blocks
-    // to reduce complexity and increase readability. Each function should have
-    // an explainetory comment like the one for this function, see row 22.
-    
-    // TODO: Remove the console.log and these comments when you've read them.
-}
+             let div1Cart = document.createElement("div"); 
+             div1Cart.className= "container mt-lg-5 text-center"; 
+             let h2Cart = document.createElement("h2");
+             h2Cart.className= "text-center"; 
+             h2Cart.innerHTML ="Customer Cart";
+             div1Cart.appendChild(h2Cart);
+             let div2Cart = document.createElement("div");
+             div2Cart.setAttribute("class", "row text-center justify-content-center");
+             div1Cart.appendChild(div2Cart); 
+             for(let i = 0; i <finalCart.length ;i++){
+                var div3Cart = document.createElement("div");
+                div3Cart.className= "col-lg-2 col-md-6"; 
+                div3Cart.setAttribute("id",finalCart[i].id);
+                var h3Cart = document.createElement("h3");
+                h3Cart.innerHTML =finalCart[i].title;
+                var h4Cart = document.createElement("h3");
+                h4Cart.innerHTML =finalCart[i].price + " Kr";
+                var removeButton = document.createElement("button");
+                removeButton.data = i
+                removeButton.className= "btn btn-danger btn-remove"; 
+
+                removeButton.onclick = function() {
+                    console.log(finalCart[i].id)
+                    var itemToRemove = document.getElementById(finalCart[i].id);
+                    div3Cart.parentNode.removeChild(itemToRemove);
+                    var cartFromStorage = JSON.parse(localStorage.getItem("cartItems"));
+                    cartFromStorage.splice(this.data, 1)
+                    localStorage.setItem("cartItems", JSON.stringify(cartFromStorage))
+
+                    let itemsTotalCount = 0;
+                    let itemsTotalAmount = 0;
+
+                    cartFromStorage.map(item => {
+                      itemsTotalCount += item.amount;
+                      itemsTotalAmount += item.price;
+                      counter.innerText = itemsTotalCount;
+                    })
+                    Storage.saveTotalAmount(itemsTotalAmount); 
+
+                    document.getElementById("totalPriceText").innerHTML = "Total Price: " + Storage.getTotalAmount() + " Kr";
+                }
+                var textButton = document.createTextNode("Remove");
+                removeButton.appendChild(textButton);
+                div3Cart.appendChild(h3Cart);
+                div3Cart.appendChild(h4Cart);
+                div3Cart.appendChild(removeButton);
+                div2Cart.appendChild(div3Cart);
+                }
+                let outerh4Cart = document.createElement("h4");
+                outerh4Cart.id = "totalPriceText"
+                outerh4Cart.innerHTML = "Total Price: " + Storage.getTotalAmount() + " Kr";
+                div1Cart.appendChild(outerh4Cart); 
+                var finishShoping = document.createElement("button");
+                finishShoping.className= "btn btn-primary btn-lg finishShoping"; 
+                finishShoping.setAttribute("data-toggle","modal");
+                finishShoping.dataset.target="#myModal";
+                finishShoping.setAttribute("type","button");
+                var finishShopingtext = document.createTextNode("Finish Shoping");
+                finishShoping.appendChild(finishShopingtext);
+                div1Cart.appendChild(finishShoping); 
+
+            result = div1Cart;
+            console.log(phoneDOM);
+            phoneDOM.innerHTML =  '' ;
+            phoneDOM.appendChild(result);
+          
+           console.log(phoneDOM);
+            
+         }
+
+
+        console.log(cartDOM);
+        phones.forEach(phone => {
+    result +=`
+    <div class="card bgcolor" style="border:${style.border}; border-bottom:${style.borderBottom};text-align:${style.textAlign}; background:${(phone.id === 102 || phone.id === 104)? style.bgcolor : style.background}">
+    <div class="card-body">
+        <h1 class="card-title" style="font-weight:${style.fontSize}">${phone.title}</h1>
+        <p class="card-text" style="font-weight:${style.fontSize}">${phone.description}</p>
+        <img  src="${phone.image}" width="200px" hight="200px">
+        <div class="card-text " style="font-weight:${style.fontSize}">${phone.price} kr</div>
+        <button type="button" class="btn btn-add-to-cart  btn-lg btn-primary mt-4 mb-5" data-id=${phone.id} ><span style="color: #ffffff;"> <i class="fa fa-cart-arrow-down mr-2"></i> </span>Add in Cart</button>
+      </div>
+    </div>         
+`;
+        });
+        
+        phoneDOM.innerHTML = result;     
+    } 
